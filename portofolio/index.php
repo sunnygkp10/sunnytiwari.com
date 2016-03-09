f1a4bd44c2134adff82a5135c61552a3838a7328
Sunny Prakash Tiwari <sunnygkp10@gmail.com>
2016-03-09 23:58:34 +0530

    project

--- portofolio/index.php
+++ portofolio/index.php
@@ -26,100 +26,255 @@ $url=$_SERVER['REQUEST_URI'];
          
      </div>
      
-     <div class="row">
-         
-         <!--card for single project-->
+    <div class="row">
+        
+          <!--card for single project-->
+         <a href="#" data-remote="false" data-toggle="modal" data-target="#profile" >
          <div class="col-xs-12 col-sm-3">
          <div class="panel panel-primary  hoverable">
                               <div class="panel-heading panel-heading-custom card-photo" >
-             <img src="http://localhost/sunnytiwari.com/image/bg.jpg" / >
+             <img src="http://localhost/sunnytiwari.com/image/capture.jpg" / >
              </div>
-            <div class="panel-body"> 
-             <p class="title1">Drishticone</p>
-                <p class="title3">KNIT News Letter</p>
-             </div>
-            
-         
-         </div>
-     </div>
-     
-         
-            <!--card for single project-->
-         <div class="col-xs-12 col-sm-3">
-         <div class="panel panel-primary">
-                              <div class="panel-heading panel-heading-custom card-photo" >
-             <img src="http://localhost/sunnytiwari.com/image/bg.jpg" / >
-             </div>
-            <div class="panel-body"> 
-             <p class="title1">Drishticone</p>
-                <p class="title3">KNIT News Letter</p>
+            <div class="panel-body" style="border-top:2px solid #202020;" > 
+             <p class="title1" style="margin-top:-3px;">Confusion (Coursera Project)</p>
+                <p class="title3">Hybrid mobile application (<span style="color:green">Ongoing</span>)</p>
              </div>
             
          
          </div>
      </div>
-     
-         
-            <!--card for single project-->
-         <div class="col-xs-12 col-sm-3">
-         <div class="panel panel-primary">
-                              <div class="panel-heading panel-heading-custom card-photo" >
-             <img src="http://localhost/sunnytiwari.com/image/bg.jpg" / >
-             </div>
-            <div class="panel-body"> 
-             <p class="title1">Drishticone</p>
-                <p class="title3">KNIT News Letter</p>
-             </div>
-            
-         
-         </div>
-     </div>
-     
-            <!--card for single project-->
-         <div class="col-xs-12 col-sm-3">
-         <div class="panel panel-primary">
-                              <div class="panel-heading panel-heading-custom card-photo" >
-             <img src="http://localhost/sunnytiwari.com/image/bg.jpg" / >
-             </div>
-            <div class="panel-body"> 
-             <p class="title1">Drishticone</p>
-                <p class="title3">KNIT News Letter</p>
-             </div>
-            
-         
-         </div>
-     </div>
-         
+             </a>
+     <!--card for single project end-->
+        
+                
           <!--card for single project-->
+         <a href="#">
          <div class="col-xs-12 col-sm-3">
-         <div class="panel panel-primary">
+         <div class="panel panel-primary  hoverable">
                               <div class="panel-heading panel-heading-custom card-photo" >
-             <img src="http://localhost/sunnytiwari.com/image/bg.jpg" / >
+             <img src="http://localhost/sunnytiwari.com/image/matching.jpg" / >
              </div>
-            <div class="panel-body"> 
-             <p class="title1">Drishticone</p>
-                <p class="title3">KNIT News Letter</p>
+            <div class="panel-body" > 
+             <p class="title1">Matching Game (Coursera)</p>
+                <p class="title3">A simple 2D javascript game</p>
              </div>
             
          
          </div>
      </div>
-         
-          <!--card for single project-->
+             </a>
+     <!--card for single project end-->
+        
+         <!--card for single project-->
+         <a href="#">
          <div class="col-xs-12 col-sm-3">
-         <div class="panel panel-primary">
+         <div class="panel panel-primary  hoverable">
                               <div class="panel-heading panel-heading-custom card-photo" >
-             <img src="http://localhost/sunnytiwari.com/image/bg.jpg" / >
+             <img src="http://localhost/sunnytiwari.com/image/dicone.jpg" / >
              </div>
-            <div class="panel-body"> 
-             <p class="title1">Drishticone</p>
-                <p class="title3">KNIT News Letter</p>
+            <div class="panel-body" style="border-top:2px solid #202020;"> 
+             <p class="title1" style="margin-top:-3px;">Drishticone </p>
+                <p class="title3">webapp for KNIT Newsletter</p>
              </div>
             
          
          </div>
      </div>
-     
-     </div></div>
+             </a>
+     <!--card for single project end-->
+         
+        
+        <!--card for single project-->
+         <a href="#">
+         <div class="col-xs-12 col-sm-3">
+         <div class="panel panel-primary  hoverable">
+                              <div class="panel-heading panel-heading-custom card-photo" >
+             <img src="http://localhost/sunnytiwari.com/image/icam.jpg" / >
+             </div>
+            <div class="panel-body" > 
+             <p class="title1">ICAM-2015</p>
+                <p class="title3">International conference's website</p>
+             </div>
+            
+         
+         </div>
+     </div>
+             </a>
+     <!--card for single project end-->
+         
+        
+        <!--card for single project-->
+         <a href="#">
+         <div class="col-xs-12 col-sm-3">
+         <div class="panel panel-primary  hoverable">
+                              <div class="panel-heading panel-heading-custom card-photo" >
+             <img src="http://localhost/sunnytiwari.com/image/exam.jpg" / >
+             </div>
+            <div class="panel-body" > 
+             <p class="title1">Online exam system</p>
+                <p class="title3">Fully dyanamic quiz</p>
+             </div>
+            
+         
+         </div>
+     </div>
+             </a>
+     <!--card for single project end-->
+         
+        
+        <!--card for single project-->
+         <a href="#">
+         <div class="col-xs-12 col-sm-3">
+         <div class="panel panel-primary  hoverable">
+                              <div class="panel-heading panel-heading-custom card-photo" >
+             <img src="http://localhost/sunnytiwari.com/image/diary.jpg" / >
+             </div>
+            <div class="panel-body" > 
+             <p class="title1">Online diary</p>
+                <p class="title3">Online personal diary</p>
+             </div>
+            
+         
+         </div>
+     </div>
+             </a>
+     <!--card for single project end-->
+         
+            <!--card for single project-->
+         <a href="#">
+         <div class="col-xs-12 col-sm-3">
+         <div class="panel panel-primary  hoverable">
+                              <div class="panel-heading panel-heading-custom card-photo" >
+             <img src="http://localhost/sunnytiwari.com/image/eff.jpg" / >
+             </div>
+            <div class="panel-body" > 
+             <p class="title1">Effulgence 2k15</p>
+                <p class="title3">KNIT Techfest website</p>
+             </div>
+            
+         
+         </div>
+     </div>
+             </a>
+     <!--card for single project end-->
+
+         
+        
+        <!--card for single project-->
+         <a href="#">
+         <div class="col-xs-12 col-sm-3">
+         <div class="panel panel-primary  hoverable">
+                              <div class="panel-heading panel-heading-custom card-photo" >
+             <img src="http://localhost/sunnytiwari.com/image/npesc.jpg" / >
+             </div>
+            <div class="panel-body" > 
+             <p class="title1">NPESC-2015</p>
+                <p class="title3">National conference's webportal</p>
+             </div>
+            
+         
+         </div>
+     </div>
+             </a>
+     <!--card for single project end-->
+         
+       <!--------------------Technical Skill Start-------------------------->
+        <div class="col-xs-12">
+          <div class="panel panel-primary" >
+                             <div  class="panel-heading panel-heading-custom" style="background: #F44336; color: #fff; font-size:20px;">Technical Skills</div>
+                              <div class="panel-body" style="padding:50px;">
+                                  <p class="title2">This section outlines my technical ability. It is not an exhaustive list however I have tried to list most of the significant technical skills that I have learned throughout my life. While I love to expand my skill-set as much as possible, I also understand the importance of having a strong command over smaller set of tools than a weak command over larger set of tools</p>
+                                  
+                                  <div class="row">
+                                      
+                                      <!--area start-->
+                                      <div class="col-xs-12 col-sm-6">
+                                      <p class="title1">Area</p>
+                                      
+                                      </div>
+                                      
+                                      <!--Favorite Tools start-->
+                                      <div class="col-xs-12 col-sm-6">
+                                      <p class="title1">Favorite Tools</p>
+                                      
+                                      </div>
+                                      
+                                      <!--area start-->
+                                      <div class="col-xs-12 col-sm-6">
+                                      <p class="title1">Area</p>
+                                      
+                                      </div>
+                                      
+                                      <!--area start-->
+                                      <div class="col-xs-12 col-sm-6">
+                                      <p class="title1">Area</p>
+                                      
+                                      </div>
+                                  
+                                  
+                                  </div>
+                                  
+                                  
+                                  
+              </div>
+            </div>
+        
+        
+        
+        
+        </div>
+         
+          
+       </div>          
+   
+     </div>
+
+
+<!--modal for project details loading-->
+<div class="modal fade" id="profile" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+   
+      <div class="modal-body" >
+          <div class="col-xs-12 col-sm-4" style="background-color:green;margin:0px">
+          <p>Con-Fusion</p>
+              <p></p>
+          
+          </div>
+          
+           <div class="col-xs-12 col-sm-8">
+          
+          <p>This section outlines my technical ability. It is not an exhaustive list however I have tried to list most of the significant technical skills that I have learned throughout my life. While I love to expand my skill-set as much as possible, I also understand the importance of having a strong command over smaller set of tools than a weak command over larger set of tools
+
+Areas
+
+Databases, Data Structures and Algorithms are among my strongest abilities. 
+I like to play around with back-end of highly scalable web applications. I also have an interest as well as sound knowledge in areas like Applied Mathematics and Distributed Systems
+Favorite Tools
+
+Operating System : Mac OS X ElCapitan, Linux (Ubuntu 15.10) 
+IDE : Eclipse, Netbeans, Codeblocks, Android Studio, Visual Studio
+Text Editor : Sublime Text, Geany, Emacs, Gedit, Vim 
+Other : Git, Latex, Gnuplot, Valgrind, GDB, Tox, Selenium, Inkscape
+
+Programming Languages
+
+Proficient : C, C++, Python (inside-out)
+Good Knowledge : Java, PHP, Shell Scripting, HTML5, XML, CSS3
+Still a lot to Learn : C#, JS, ASM, R, Ruby 
+Programming Language is just a tool, Logic is main art.
+Libraries & Frameworks
+
+Web : Django, Rails, NodeJS, JQuery, Bootstrap, MySQL 
+Libraries : STL, Cuda, OpenMP, MPI, BLAS, Boost 
+Devices : Android SDK, Microsoft SDK, Kinect SDK, PhoneGap 
+Others : Map-Reduce (Hadoop), Storm, RabbitMQ, Celery, AdSense 
+</p>
+          </div>
+    
+   </div>
+    </div>
+  </div>
+</div>
 
 <?php include '../footer.php' ?>
